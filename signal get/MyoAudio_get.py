d4b37ab068edd31969cf95cffd1d709a030ab183
b1013252@fun.ac.jp <b1013252@fun.ac.jp>
2016-12-06 11:59:14 +0900

    main program

--- signal get/MyoAudio_get.py
+++ signal get/MyoAudio_get.py
@@ -7,13 +7,16 @@ import serial
 import threading
 from time import time
 
-#test
-RECORD_SECONDS = 1 #rec time
+#settings
+RECORD_SECONDS = 1 #rec time[sec]
 bpm = 150 #beat per minute
-beeptime = 100 #click time
+beeptime = 100 #click time[msec]
 
-bpm_time = 1000.0/(bpm / 60) #time/1click msec
-minus_beeptime = (bpm_time - beeptime)/1000.0 #wait time between clicks sec
+bpm_time = 1000.0/(bpm / 60) #time/1click[msec]
+minus_beeptime = (bpm_time - beeptime)/1000.0 #wait time between clicks[sec]
+
+WAVE_OUTPUT_FILENAME = "../data/Audio/file.wav" #wav file name
+MYO_OUTPUT_FILENAME = "../data/Myo/Myo_file.txt" #myo file name
 
 #---audio setup---
 input_num = 1 #マイク入力のデバイス番号
@@ -22,95 +25,100 @@ FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 CHUNK = 2**11
-WAVE_OUTPUT_FILENAME = "file.wav"
 
 audio = pyaudio.PyAudio()
 Audio_Frames = []
 Myo_Frames = []
 def Audio_Callback(in_data, frame_count, time_info, status):
-    Audio_Frames.append(in_data)          #この中で別スレッドの処理
+    Audio_Frames.append(in_data) #この中で別スレッドの処理
     return(None, pyaudio.paContinue)
 
 #myoelectronical(arduino) setup
-ser = serial.Serial("COM3",115200)  # デバイス名とボーレートを設定しポートをオープン
+ser = serial.Serial("COM3",115200) #デバイス名とボーレートを設定しポートをオープン
 
 def Myo_get():
     while stream.is_active():
         data = ser.read()
-        Myo_Frames.append(data)          #この中で別スレッドの処理
+        Myo_Frames.append(data)
 
-th_me = threading.Thread(target = Myo_get, name = "th_me")
+th_Myo = threading.Thread(target = Myo_get, name = "th_me")
 
-#---rec part---
-raw_input("press Enter key")
-
-i = 0
-
-#audio & myoelectronical stream start
-stream = audio.open(format=FORMAT, channels=CHANNELS,
-                rate=RATE, input=True,
-                input_device_index=input_num,
-                frames_per_buffer=CHUNK,
-                stream_callback=Audio_Callback)
-start_time = time()
-th_me.start()
-
-#countdown to rec start
-for count in [4,3,2,1]:
-    print count
+#click set up
+def click_output():
     Beep(700,beeptime)
     sleep(minus_beeptime)
 
-print ("recording...")
-#stream.start_stream()
+th_click = threading.Thread()
 
-while stream.is_active():
-    Beep(700,beeptime)
-    sleep(minus_beeptime)
-    i += 1
-    if i/(bpm / 60) >= RECORD_SECONDS:
-        break
+if __name__ == "__main__":
+    #---rec part---
+    raw_input("press Enter key for rec start")
 
-#sleep(RECORD_SECONDS)
-#raw_imput("end")
-print ("finished recording")
-print ("wait...")
-#bottom margin
-for count in [4,3,2,1]:
-    sleep(minus_beeptime)
+    #audio & myoelectronical stream start
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                    rate=RATE, input=True,
+                    input_device_index=input_num,
+                    frames_per_buffer=CHUNK,
+                    stream_callback=Audio_Callback)
+    start_time = time()
+    th_Myo.start()
 
-#---output part---
-stream.stop_stream()
-stream.close()
-audio.terminate()
+    #countdown to rec start
+    for count in [4,3,2,1]:
+        print count
+        Beep(700,beeptime)
+        sleep(minus_beeptime)
 
-th_me._Thread__stop()
+    print ("recording...")
+    #stream.start_stream()
+    """
+    click_count = 0
+    while stream.is_active():
+        if click_count/(bpm / 60) >= RECORD_SECONDS: #指定秒数でループ終了
+            break
+        Beep(700,beeptime)
+        sleep(minus_beeptime)
+        click_count += 1
+    """
+    #raw_imput("press enter key for rec end") #
+    print ("finished recording")
+    print ("wait...")
+    #bottom margin
+    for count in [4,3,2,1]:
+        sleep(minus_beeptime)
 
-end_time = time()
+    #---output part---
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
 
-ser.close() # ポートのクローズ
+    th_Myo._Thread__stop()
 
-#Audio output
-waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-waveFile.setnchannels(CHANNELS)
-waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-waveFile.setframerate(RATE)
-waveFile.writeframes(b''.join(Audio_Frames))
-waveFile.close()
+    end_time = time()
 
-#Myoelectronical output
-f = open("Myo_file.txt", "w")
+    ser.close() #ポートのクローズ
 
-outliers = "・" #外れ値群　""の中にどんどん追加していく書式
+    #Audio output
+    waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    waveFile.setnchannels(CHANNELS)
+    waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+    waveFile.setframerate(RATE)
+    waveFile.writeframes(b''.join(Audio_Frames))
+    waveFile.close()
 
-#f.write(Myo_Frames[0].translate(None, outliers))
-Myo_SR = len(Myo_Frames)//(end_time - start_time) #Myoelectronical sampling rate
+    #Myoelectronical output
+    f = open(MYO_OUTPUT_FILENAME, "w")
 
-f.write(str(Myo_SR))
-for row in Myo_Frames:
-    #write_data = row
-    f.write("\n") #最初だけ改行無し
-    f.write(row.translate(None, "・")) #外れ値削除しファイルに書き込み
+    outliers = "・" #外れ値群　""の中にどんどん追加していく書式
 
-    #print row
-print "finished"
+    #f.write(Myo_Frames[0].translate(None, outliers))
+    Myo_SR = len(Myo_Frames)//(end_time - start_time) #Myoelectronical sampling rate
+
+    f.write(str(Myo_SR))
+    for row in Myo_Frames:
+        #write_data = row
+        f.write("\n") #最初だけ改行無し
+        f.write(row.translate(None, "・")) #外れ値削除しファイルに書き込み
+
+        #print row
+    print "finished"
