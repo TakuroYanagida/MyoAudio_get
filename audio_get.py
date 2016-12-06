c44b4ae426d9fae24f19587d9d5e5ccafce180fc
b1013252@fun.ac.jp <b1013252@fun.ac.jp>
2016-12-06 09:28:36 +0900

    Confirmation Audio get

--- audio_get.py
+++ audio_get.py
@@ -37,18 +37,13 @@ def callback(in_data, frame_count, time_info, status):
     return(None, pyaudio.paContinue)
 
 raw_input("press Enter key")
-print("4")
-Beep(700,100)
-sleep(0.9)
-print("3")
-Beep(700,100)
-sleep(0.9)
-print("2")
-Beep(700,100)
-sleep(0.9)
-print("1")
-Beep(700,100)
-sleep(0.9)
+
+#countdown to rec start
+for count in [4,3,2,1]:
+    print count
+    Beep(700,beeptime)
+    sleep(minus_beeptime)
+
 print ("recording...")
 #stream.start_stream()
 stream = audio.open(format=FORMAT, channels=CHANNELS,
@@ -69,60 +64,3 @@ waveFile.setsampwidth(audio.get_sample_size(FORMAT))
 waveFile.setframerate(RATE)
 waveFile.writeframes(b''.join(frames))
 waveFile.close()
-
-"""
-import pyaudio
-import wave
-import time
-
-imput_num1 = 1
-imput_num2 = 9
-
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-CHUNK = 2**11
-RECORD_SECONDS = 10
-WAVE_OUTPUT_FILENAME1 = "file1.wav"
-WAVE_OUTPUT_FILENAME2 = "file2.wav"
-
-audio = pyaudio.PyAudio()
-frames1 = []
-frames2 = []
-def callback(in_data, frame_count, time_info, status):
-    frames.append(in_data)          #この中で別スレッドの処理
-    return(None, pyaudio.paContinue)
-
-stream = audio.open(format=FORMAT, channels=CHANNELS,
-                rate=RATE, input=True,
-                input_device_index=input_num,
-                frames_per_buffer=CHUNK,
-                stream_callback=callback)
-
-print ("recording...")
-stream.start_stream()
-time.sleep(RECORD_SECONDS)
-print ("finished recording")
-
-stream.stop_stream()
-stream.close()
-audio.terminate()
-
-waveFile1 = wave.open(WAVE_OUTPUT_FILENAME1, 'wb')
-waveFile2 = wave.open(WAVE_OUTPUT_FILENAME2, 'wb')
-
-waveFile1.setnchannels(CHANNELS)
-waveFile2.setnchannels(CHANNELS)
-
-waveFile1.setsampwidth(audio.get_sample_size(FORMAT))
-waveFile2.setsampwidth(audio.get_sample_size(FORMAT))
-
-waveFile1.setframerate(RATE)
-waveFile2.setframerate(RATE)
-
-waveFile1.writeframes(b''.join(frames1))
-waveFile2.writeframes(b''.join(frames2))
-
-waveFile1.close()
-waveFile2.close()
-"""
