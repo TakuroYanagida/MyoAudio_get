1e0005e2a7e42bcb734a27bac50c31344e4140ee
b1013252@fun.ac.jp <b1013252@fun.ac.jp>
2016-12-06 13:58:09 +0900

    main program

--- signal get/MyoAudio_get.py
+++ signal get/MyoAudio_get.py
@@ -7,16 +7,56 @@ import serial
 import threading
 from time import time
 
-#settings
-RECORD_SECONDS = 1 #rec time[sec]
+#---settings---
+#RECORD_SECONDS = 1 #rec time[sec]
 bpm = 150 #beat per minute
 beeptime = 100 #click time[msec]
 
 bpm_time = 1000.0/(bpm / 60) #time/1click[msec]
 minus_beeptime = (bpm_time - beeptime)/1000.0 #wait time between clicks[sec]
 
-WAVE_OUTPUT_FILENAME = "../data/Audio/file.wav" #wav file name
-MYO_OUTPUT_FILENAME = "../data/Myo/Myo_file.txt" #myo file name
+WAVE_OUTPUT_FILENAME = "../Audio_file.wav" #wav file name
+MYO_OUTPUT_FILENAME = "../Myo_file.txt" #myo file name
+
+Myo_Frames = [] #Myoelectronical signal colums
+Audio_Frames = [] #Audio signal colums
+
+#define thread class
+class control_th():
+
+    def __init__(self):
+        self.stop_event = threading.Event() #停止させるかのフラグ
+
+        #スレッドの作成と開始
+        self.thread_Myo = threading.Thread(target = self.Myo_get)
+        self.thread_click = threading.Thread(target = self.click_output)
+        self.thread_Myo.start()
+
+        #head margin countdown to rec start
+        for count in [4,3,2,1]:
+            print count
+            Beep(700,beeptime)
+            sleep(minus_beeptime)
+
+        self.thread_click.start()
+
+    def Myo_get(self):
+        count = 1
+        step = 1
+        while not self.stop_event.is_set():
+            data = ser.read()
+            Myo_Frames.append(data)
+
+    def click_output(self):
+        while not self.stop_event.is_set():
+            Beep(700,beeptime)
+            sleep(minus_beeptime)
+
+    def stop(self):
+        """スレッドを停止させる"""
+        self.stop_event.set()
+        self.thread_Myo.join()    #スレッドが停止するのを待つ
+        self.thread_click.join()    #スレッドが停止するのを待つ
 
 #---audio setup---
 input_num = 1 #マイク入力のデバイス番号
@@ -26,9 +66,6 @@ CHANNELS = 1
 RATE = 44100
 CHUNK = 2**11
 
-audio = pyaudio.PyAudio()
-Audio_Frames = []
-Myo_Frames = []
 def Audio_Callback(in_data, frame_count, time_info, status):
     Audio_Frames.append(in_data) #この中で別スレッドの処理
     return(None, pyaudio.paContinue)
@@ -36,42 +73,28 @@ def Audio_Callback(in_data, frame_count, time_info, status):
 #myoelectronical(arduino) setup
 ser = serial.Serial("COM3",115200) #デバイス名とボーレートを設定しポートをオープン
 
-def Myo_get():
-    while stream.is_active():
-        data = ser.read()
-        Myo_Frames.append(data)
-
-th_Myo = threading.Thread(target = Myo_get, name = "th_me")
-
-#click set up
-def click_output():
-    Beep(700,beeptime)
-    sleep(minus_beeptime)
-
-th_click = threading.Thread()
-
 if __name__ == "__main__":
+    audio = pyaudio.PyAudio()
+
     #---rec part---
     raw_input("press Enter key for rec start")
 
     #audio & myoelectronical stream start
-    stream = audio.open(format=FORMAT, channels=CHANNELS,
-                    rate=RATE, input=True,
-                    input_device_index=input_num,
-                    frames_per_buffer=CHUNK,
-                    stream_callback=Audio_Callback)
-    start_time = time()
-    th_Myo.start()
+    stream = audio.open(format = FORMAT, channels = CHANNELS,
+                        rate = RATE, input = True,
+                        input_device_index = input_num,
+                        frames_per_buffer = CHUNK,
+                        stream_callback = Audio_Callback)
+    th_start_time = time() #thread start time
 
-    #countdown to rec start
-    for count in [4,3,2,1]:
-        print count
-        Beep(700,beeptime)
-        sleep(minus_beeptime)
+    #thread start
+    thread_do = control_th()
 
+    #th_Myo.start()
     print ("recording...")
     #stream.start_stream()
     """
+    #指定秒数で録音終了
     click_count = 0
     while stream.is_active():
         if click_count/(bpm / 60) >= RECORD_SECONDS: #指定秒数でループ終了
@@ -80,23 +103,22 @@ if __name__ == "__main__":
         sleep(minus_beeptime)
         click_count += 1
     """
-    th_click.start()
     raw_input("press enter key for rec end") #rec end by press enter
     print ("finished recording")
     print ("wait...")
+
     #bottom margin
     for count in [4,3,2,1]:
         sleep(minus_beeptime)
-    th_click._Thread__stop()
+
+    #thread end
+    thread_do.stop()
+    th_end_time = time() #thread end time
 
     #---output part---
     stream.stop_stream()
     stream.close()
     audio.terminate()
-
-    th_Myo._Thread__stop()
-
-    end_time = time()
 
     ser.close() #ポートのクローズ
 
@@ -114,12 +136,12 @@ if __name__ == "__main__":
     outliers = "・" #外れ値群　""の中にどんどん追加していく書式
 
     #f.write(Myo_Frames[0].translate(None, outliers))
-    Myo_SR = len(Myo_Frames)//(end_time - start_time) #Myoelectronical sampling rate
+    Myo_SR = len(Myo_Frames)//(th_end_time - th_start_time) #Myoelectronical sampling rate
 
-    f.write(str(Myo_SR))
+    f.write(str(Myo_SR)) #write sampling rate
     for row in Myo_Frames:
         #write_data = row
-        f.write("\n") #最初だけ改行無し
+        f.write("\n")
         f.write(row.translate(None, "・")) #外れ値削除しファイルに書き込み
 
         #print row
